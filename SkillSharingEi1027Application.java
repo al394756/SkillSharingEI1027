a4887b20b44ccbbc2bbaa7bba2458ea9b753cd4e
al394756 <al394756@uji.es>
2022-02-26 17:12:39 +0100

    Creado git

---
+++ SkillSharingEi1027Application.java
@@ -0,0 +1,13 @@
+package com.example.SkillSharingEI1027;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillSharingEi1027Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkillSharingEi1027Application.class, args);
+	}
+
+}
