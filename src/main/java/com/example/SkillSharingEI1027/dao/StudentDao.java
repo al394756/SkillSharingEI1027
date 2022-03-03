5def9d6e2506db1569e54333fd5a3a3bc323c532
al394756 <al394756@uji.es>
2022-03-03 16:56:04 +0100

    Añadido StudentDao

--- src/main/java/com/example/SkillSharingEI1027/dao/StudentDao.java
+++ src/main/java/com/example/SkillSharingEI1027/dao/StudentDao.java
@@ -1,4 +1,52 @@
 package com.example.SkillSharingEI1027.dao;
 
+import com.example.SkillSharingEI1027.modelo.Student;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
 public class StudentDao {
+    private JdbcTemplate jdbcTemplate;
+
+    //Obtiene el jdbc a partir de Data Source
+    @Autowired
+    public void setDataSource(DataSource dataSource) {
+        jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    //Añade el Student a la BBDD
+    public void addStudent(Student student){
+        jdbcTemplate.update("INSERT INTO Student VALUES(?,?,?,?,?,?,?,?,?,?,?",student.getIdStudent(),student.getDni(),student.getName(),student.getEmail(),
+        student.getPhoneNumber(),student.getPassword(),student.getDegree(),student.getCourse(),student.getBalanceHours(),student.isSkpMember(),student.isActiveAccount());
+    }
+
+    //No se puede borrar estudiante, solo podemos cancelar su cuenta poniendo activeAccount a false
+    public void cancelStudent(Student student){
+        jdbcTemplate.update("UPDATE Student SET activeAccount=false WHERE idStudent=?",student.getIdStudent());
+    }
+
+    //Obtenemos Student con su id. Devuelve null si no existe o si la cuenta está inactiva
+    public Student getStudent(String idStudent){
+        try{
+            return jdbcTemplate.queryForObject("SELECT * FROM Student WHERE idStudent = ?, activeAccount=true", new StudentRowMapper(), idStudent);
+        } catch (EmptyResultDataAccessException e){
+            return null;
+        }
+    }
+
+    //Obtenemos todos los students con cuentas activas o null si no hay ninguno
+
+    public List<Student> getStudentsActivos(){
+        try{
+            return jdbcTemplate.query("SELECT * FROM Student WHERE activeAccount=true", new StudentRowMapper());
+        } catch (EmptyResultDataAccessException e){
+            return new ArrayList<>();
+        }
+    }
 }
