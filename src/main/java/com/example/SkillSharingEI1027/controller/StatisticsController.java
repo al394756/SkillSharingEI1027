e4ca4a70dd7a4307f83e7b77bcb43e9913d4c85d
pxe05 <79581464+pxe05@users.noreply.github.com>
2022-05-29 19:16:15 +0200

    prvencion de errores statistics

--- src/main/java/com/example/SkillSharingEI1027/controller/StatisticsController.java
+++ src/main/java/com/example/SkillSharingEI1027/controller/StatisticsController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -29,25 +30,48 @@ public class StatisticsController {
 
     @RequestMapping("/statistics")
     public String statistics(Model model){
-        float pOfDDistinctSkillsOffer=statisticsDao.numberOfDistinctSkillsInOffeRequest("offer");
-        float pOfDDistinctSkillsRequest=statisticsDao.numberOfDistinctSkillsInOffeRequest("request");
-        float pOfDDistinctSkillsCollaboration=statisticsDao.numberOfDistinctSkillsInCollaboration();
+        float pOfDDistinctSkillsOffer;
+        float pOfDDistinctSkillsRequest;
+        float pOfDDistinctSkillsCollaboration;
+        if(skillDao.getSkills().isEmpty()){
+            pOfDDistinctSkillsOffer=0;
+            pOfDDistinctSkillsRequest=0;
+            pOfDDistinctSkillsCollaboration=0;
+        }
+        else {
+            pOfDDistinctSkillsOffer = statisticsDao.numberOfDistinctSkillsInOffeRequest("offer");
+            pOfDDistinctSkillsRequest = statisticsDao.numberOfDistinctSkillsInOffeRequest("request");
+            pOfDDistinctSkillsCollaboration = statisticsDao.numberOfDistinctSkillsInCollaboration();
+        }
 
         model.addAttribute("pOfDDistinctSkillsOffer",String.format("%.2f", pOfDDistinctSkillsOffer));
         model.addAttribute("pOfDDistinctSkillsRequest",String.format("%.2f", pOfDDistinctSkillsRequest));
         model.addAttribute("pOfDDistinctSkillsCollaboration",String.format("%.2f",pOfDDistinctSkillsCollaboration));
 
-        List<Statistics> timesOfSkillUsedOffer=statisticsDao.timesOfSkillUsedOffeRequest("offer");
-        List<Statistics> timesOfSkillUsedRequest=statisticsDao.timesOfSkillUsedOffeRequest("request");
-        List<Statistics> timesOfSkillUsedCollaboration=statisticsDao.timesOfSkillUsedCollaboration();
+        List<Statistics> timesOfSkillUsedOffer=new ArrayList<>();
+        List<Statistics> timesOfSkillUsedRequest=new ArrayList<>();
+        List<Statistics> timesOfSkillUsedCollaboration=new ArrayList<>();
+
+        List<Statistics> timesMVPStudentOffer=new ArrayList<>();
+        List<Statistics> timesMVPStudentRequest=new ArrayList<>();
+        List<Statistics> timesMVPStudentCollaboration=new ArrayList<>();
+
+        if (!offeRequestDao.getActiveOffeRequests("offer").isEmpty()){
+            timesOfSkillUsedOffer=statisticsDao.timesOfSkillUsedOffeRequest("offer");
+            timesMVPStudentOffer=statisticsDao.timesMVPStudentOffeRequest("offer");
+        }
+        if (!offeRequestDao.getActiveOffeRequests("request").isEmpty()){
+            timesOfSkillUsedRequest=statisticsDao.timesOfSkillUsedOffeRequest("request");
+            timesMVPStudentRequest=statisticsDao.timesMVPStudentOffeRequest("request");
+        }
+        if (!collaborationDao.getCollaborationsActivas().isEmpty()){
+            timesOfSkillUsedCollaboration=statisticsDao.timesOfSkillUsedCollaboration();
+            timesMVPStudentCollaboration=statisticsDao.timesMVPStudentCollaboration();
+        }
 
         model.addAttribute("timesOfSkillUsedOffer",timesOfSkillUsedOffer);
         model.addAttribute("timesOfSkillUsedRequest",timesOfSkillUsedRequest);
         model.addAttribute("timesOfSkillUsedCollaboration",timesOfSkillUsedCollaboration);
-
-        List<Statistics> timesMVPStudentOffer=statisticsDao.timesMVPStudentOffeRequest("offer");
-        List<Statistics> timesMVPStudentRequest=statisticsDao.timesMVPStudentOffeRequest("request");
-        List<Statistics> timesMVPStudentCollaboration=statisticsDao.timesMVPStudentCollaboration();
 
         model.addAttribute("timesMVPStudentOffer",timesMVPStudentOffer);
         model.addAttribute("timesMVPStudentRequest",timesMVPStudentRequest);
